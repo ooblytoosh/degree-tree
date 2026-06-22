dc5b74526e60487fb6e2a034d6d4072256154eb2
aayushmpatra <aayushmpatra2025@gmail.com>
2026-06-21 22:05:11 -0500

    added implied postrequisite removal
    added feature where deselecting a lower level class also deselects all classes that require it and its postrequisites

--- src/App.jsx
+++ src/App.jsx
@@ -40,28 +40,45 @@ function App() {
     }
   }
 
+  const handleRemoveCourse = courseId => {
+    const next = new Set(takenCourses);
+    const queue = [courseId];
+
+    while (queue.length > 0) {
+      const current = queue.shift();
+
+      if (next.has(current)) {
+        next.delete(current);
+      }
+
+      Object.keys(COURSES).forEach(id => {
+        if (COURSES[id].prereqs.includes(current)) {
+          queue.push(id);
+        }
+      })
+    }
+
+    setTakenCourses(next);
+  }
+
   return (
     <div>
       <h1>prereqd</h1>
       <div className="lists">
         <div className="taken">
-          <h2>Taken</h2>
+          <h2>Taken: {takenCourses.size}</h2>
           {[...takenCourses].map(courseId => 
-            <CourseCard key={courseId} courseId={courseId} status='taken' onClick={() => {
-              const next = new Set(takenCourses);
-              next.delete(courseId);
-              setTakenCourses(next);
-            }}/>
+            <CourseCard key={courseId} courseId={courseId} status='taken' onClick={() => handleRemoveCourse(courseId)}/>
           )}
         </div>
         <div>
-          <h2>Available</h2>
+          <h2>Available: {availableCourses.length}</h2>
           {[...availableCourses].map(courseId => 
             <CourseCard key={courseId} courseId={courseId} status='available' onClick={() => setTakenCourses(new Set([...takenCourses, courseId]))}/>
           )}
         </div>
         <div>
-          <h2>Locked</h2>
+          <h2>Locked: {lockedCourses.length}</h2>
           {[...lockedCourses].map(courseId => 
             <CourseCard key={courseId} courseId={courseId} status='locked' />
           )}
